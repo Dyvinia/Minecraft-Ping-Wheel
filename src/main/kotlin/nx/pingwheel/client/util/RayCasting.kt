e93bc8cbf3835ff2f0b5e4b56ffc1f9aada7f8d1
Dyvine57 <dulana57@gmail.com>
2023-03-27 15:45:19 -0400

    Change previous change to only LivingEntities

--- src/main/kotlin/nx/pingwheel/client/util/RayCasting.kt
+++ src/main/kotlin/nx/pingwheel/client/util/RayCasting.kt
@@ -1,7 +1,7 @@
 package nx.pingwheel.client.util
 
 import net.minecraft.entity.Entity
-import net.minecraft.entity.mob.MobEntity
+import net.minecraft.entity.LivingEntity
 import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
 import net.minecraft.util.math.Box
@@ -73,7 +73,7 @@ object RayCasting {
 			rayStartVec,
 			rayEndVec,
 			boundingBox,
-		) { targetEntity -> !targetEntity.isSpectator && targetEntity is MobEntity } ?: blockHitResult
+		) { targetEntity -> !targetEntity.isSpectator && targetEntity is LivingEntity } ?: blockHitResult
 
 		if (rayStartVec.squaredDistanceTo(entityHitResult.pos) < rayStartVec.squaredDistanceTo(blockHitResult.pos)) {
 			return entityHitResult
